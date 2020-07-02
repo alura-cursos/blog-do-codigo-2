2a9f725f60a589adf25f26824444d84c2d6737f8
Andrew Ijano <andrew.ijano@gmail.com>
2020-07-02 20:30:39 -0300

    Video 2.4

--- src/usuarios/middlewares-autenticacao.js
+++ src/usuarios/middlewares-autenticacao.js
@@ -1,4 +1,22 @@
 const passport = require('passport');
+const Usuario = require('./usuarios-modelo');
+const {InvalidArgumentError} = require('../erros');
+const allowlistRefreshToken = require('../../redis/allowlist-refresh-token');
+
+async function verificaRefreshToken(refreshToken) {
+  if (!refreshToken) {
+    throw new InvalidArgumentError('Refresh não enviado!');
+  }
+  const id = await allowlistRefreshToken.buscaValor(refreshToken);
+  if (!id) {
+    throw new InvalidArgumentError('Refresh token inválido!');
+  }
+  return id;
+}
+
+async function invalidaRefreshToken(refreshToken) {
+  await allowlistRefreshToken.deleta(refreshToken);
+}
 
 module.exports = {
   local(req, res, next) {
@@ -52,5 +70,20 @@ module.exports = {
         return next();
       }
     )(req, res, next);
+  },
+
+  async refresh(req, res, next) {
+    try {
+      const { refreshToken } = req.body;
+      const id = await verificaRefreshToken(refreshToken);
+      await invalidaRefreshToken(refreshToken);
+      req.user = await Usuario.buscaPorId(id);
+      return next();
+    } catch (erro) {
+      if (erro.name === 'InvalidArgumentError') {
+        return res.status(401).json({erro: erro.message});
+      }
+      return res.status(500).json({erro: erro.message});
+    }
   },
 };
