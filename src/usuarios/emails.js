86af1551063e951b37ea4a7efb6c1f3d8fca3a03
Andrew Ijano <andrew.ijano@gmail.com>
2020-07-14 22:32:12 -0300

    Video 4.3

--- src/usuarios/emails.js
+++ src/usuarios/emails.js
@@ -1,20 +1,27 @@
 const nodemailer = require('nodemailer');
 
-async function enviaEmail(usuario) {
-  const contaTeste = await nodemailer.createTestAccount();
-  const transportador = nodemailer.createTransport({
-    host: 'smtp.ethereal.email',
-    auth: contaTeste,
-  });
-  const info = await transportador.sendMail({
-    from: '"Blog do Código" <noreply@blogdocodigo.com.br>',
-    to: usuario.email,
-    subject: 'Teste de e-mail',
-    text: 'Olá! Este é um e-mail de teste!',
-    html: '<h1>Olá!</h1> <p>Este é um e-mail de teste!</p>',
-  });
-
-  console.log('URL: ' + nodemailer.getTestMessageUrl(info));
+class Email {
+  async enviaEmail() {
+    const contaTeste = await nodemailer.createTestAccount();
+    const transportador = nodemailer.createTransport({
+      host: 'smtp.ethereal.email',
+      auth: contaTeste,
+    });
+    const info = await transportador.sendMail(this);
+  
+    console.log('URL: ' + nodemailer.getTestMessageUrl(info));
+  }
 }
 
-module.exports = { enviaEmail };
+class EmailVerificacao extends Email {
+  constructor(usuario, endereco) {
+    super();
+    this.from = '"Blog do Código" <noreply@blogdocodigo.com.br>';
+    this.to = usuario.email;
+    this.subject = 'Verificação de e-mail';
+    this.text = `Olá! Verifique seu e-mail aqui: ${endereco}`;
+    this.html = `<h1>Olá!</h1> Verifique seu e-mail aqui: <a href="${endereco}">${endereco}</a>`;
+  }
+}
+
+module.exports = { EmailVerificacao };
--- src/usuarios/usuarios-controlador.js
+++ src/usuarios/usuarios-controlador.js
@@ -2,7 +2,7 @@ const Usuario = require('./usuarios-modelo');
 const { InvalidArgumentError } = require('../erros');
 
 const tokens = require('./tokens');
-const emails = require('./emails');
+const { EmailVerificacao } = require('./emails');
 
 module.exports = {
   async adiciona(req, res) {
@@ -16,8 +16,10 @@ module.exports = {
       await usuario.adicionaSenha(senha);
       await usuario.adiciona();
 
-      emails.enviaEmail(usuario).catch(console.log);
-      
+      const endereco = 'localhost:3000/usuario/verifica_email/' + usuario.id;
+      const emailVerificacao = new EmailVerificacao(usuario, endereco);
+      emailVerificacao.enviaEmail().catch(console.log);
+
       res.status(201).json();
     } catch (erro) {
       if (erro instanceof InvalidArgumentError) {
